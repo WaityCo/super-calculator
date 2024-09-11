8ad63ee4943d7b2637c69a49161b6532e35f4f73
luna1860 <saritaprinmc@gmail.com>
2024-09-11 09:29:55 -0500

    Mejora de menú, suma y parcialmemte multiplicación

--- super_calculator.py
+++ super_calculator.py
@@ -1,20 +1,33 @@
 while True:
+    print("========================")
     print("1. Sumar")
     print("2. Restar")
     print("3. Multiplicar")
     print("4. Dividir")
+    print("========================")
 
+    print("")
     user_input = int(input("Ingresa tu opción aqui => "))
 
     if user_input == 1:
-        num1_suma = input("Ingresa el primero numero: ")
-        num2_suma = input("Ingrese el segundo numero: ")
+        print("========================================")
+        print("")
+        num1_suma = int(input("Ingresa el primero numero: "))
+        num2_suma = int(input("Ingrese el segundo numero: "))
+        print("========================================")
 
         def suma(num1_suma, num2_suma):
-            return num1_suma + num2_suma 
+            return num1_suma + num2_suma
 
-        resultado_suma = def suma(num1_suma, num2_suma):
+        resultado_suma = suma(num1_suma, num2_suma)
+        print("")
         print(resultado_suma)
+        print("")
 
-        
+    elif user_input == 3:
+        print("========================================")
+        print("")
+        num1_multiplicar = int(input("Ingrese el primer numero "))
+        num2_multiplicar = int(input("Ingrese el segundo numero "))
 
+        def multiplicar(num1_multiplicar, )
